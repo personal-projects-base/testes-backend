6796d068973254a8f90fc18a1f3ad19582ca9aef
Geovane Araújo <geovane-araujo@outlook.com>
2023-08-27 19:21:35 -0300

    testes

--- src/main/resources/db/migration/V20230826181135001__Create_entities.sql
+++ src/main/resources/db/migration/V20230826181135001__Create_entities.sql
@@ -5,23 +5,35 @@ CREATE TABLE IF NOT EXISTS city(
                                    id uuid,
                                    name varchar,
                                    code varchar,
-                                   state uuid
-);
+                                   state uuid,
+                                   created_by varchar(80),
+    created_date timestamp,
+    last_modified_by varchar(80),
+    last_modified_date timestamp
+    );
 
 CREATE TABLE IF NOT EXISTS state(
 
                                     id uuid,
                                     name varchar,
                                     code varchar,
-                                    country uuid
-);
+                                    country uuid,
+                                    created_by varchar(80),
+    created_date timestamp,
+    last_modified_by varchar(80),
+    last_modified_date timestamp
+    );
 
 CREATE TABLE IF NOT EXISTS country(
 
                                       id uuid,
                                       name varchar,
-                                      code varchar
-);
+                                      code varchar,
+                                      created_by varchar(80),
+    created_date timestamp,
+    last_modified_by varchar(80),
+    last_modified_date timestamp
+    );
 
 CREATE TABLE IF NOT EXISTS pessoas(
 
@@ -30,17 +42,21 @@ CREATE TABLE IF NOT EXISTS pessoas(
                                       logradouro varchar,
                                       bairo varchar,
                                       cep varchar,
-                                      cidade uuid
-);
+                                      cidade uuid,
+                                      created_by varchar(80),
+    created_date timestamp,
+    last_modified_by varchar(80),
+    last_modified_date timestamp
+    );
 
 -- PKs
 
-ALTER TABLE city  ADD CONSTRAINT ok_R7ovNX5RemDU5BymT0jC  PRIMARY KEY (id);
-ALTER TABLE state  ADD CONSTRAINT ok_TxgAVcSxBh93FvGKdJ9c  PRIMARY KEY (id);
-ALTER TABLE country  ADD CONSTRAINT ok_6sfFnBWzagBzouzgogQM  PRIMARY KEY (id);
-ALTER TABLE pessoas  ADD CONSTRAINT ok_P98a7gPnaUK07LRKlS9H  PRIMARY KEY (id);
+ALTER TABLE city  ADD CONSTRAINT ok_fttCY65PRHT4rqfbbmcb  PRIMARY KEY (id);
+ALTER TABLE state  ADD CONSTRAINT ok_bfoSUhDX21Knqoqsboe4  PRIMARY KEY (id);
+ALTER TABLE country  ADD CONSTRAINT ok_yuRL3pMuVQghqcwRr68f  PRIMARY KEY (id);
+ALTER TABLE pessoas  ADD CONSTRAINT ok_ld8iiDM7Lisdqbwjgeuw  PRIMARY KEY (id);
 -- Fks
 
-ALTER TABLE city ADD CONSTRAINT fk_ymFLLjTe92Nnn24ZaQBO FOREIGN KEY (state) REFERENCES state(id);
-ALTER TABLE state ADD CONSTRAINT fk_KORZ3yRBXby6Bb83cXMI FOREIGN KEY (country) REFERENCES country(id);
-ALTER TABLE pessoas ADD CONSTRAINT fk_Lnj17Z6OAznsmKWEIC8D FOREIGN KEY (cidade) REFERENCES city(id);
+ALTER TABLE city ADD CONSTRAINT fk_C4fpyh3CJewUG8bSi2mU FOREIGN KEY (state) REFERENCES state(id);
+ALTER TABLE state ADD CONSTRAINT fk_nwlhOUZKgGE8oHEDG31o FOREIGN KEY (country) REFERENCES country(id);
+ALTER TABLE pessoas ADD CONSTRAINT fk_3rMU08d3P6rMZutwZd2Y FOREIGN KEY (cidade) REFERENCES city(id);
